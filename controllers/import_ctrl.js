687acf538760b4fbf417331e81286a8c7e3cad27
clementkoenig <clement.koenig@unistra.fr>
2020-02-28 11:27:48 +0100

    Import flags

--- controllers/import_ctrl.js
+++ controllers/import_ctrl.js
@@ -1,6 +1,6 @@
 const initialValues = require('../config.json'),
-    { Config, Team, Area } = require('../models'),
-    { config_store, team_store, area_store } = require('../stores');
+    { Config, Team, Area, Flag } = require('../models'),
+    { config_store, team_store, area_store, flag_store } = require('../stores');
 
 module.exports = {
     import_config: () => {
@@ -8,5 +8,6 @@ module.exports = {
 
         initialValues.Teams.map(t => team_store.add(new Team(t)));
         initialValues.Areas.map(a => area_store.add(new Area(a)));
+        initialValues.Flags.map(f => flag_store.add(new Flag(f)));
     }
 };
