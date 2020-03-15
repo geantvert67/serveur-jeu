a23b27cb7628817cbd310e77e3420dbb79c6e778
Clément KOENIG <clement.koenig@unistra.fr>
2020-03-15 15:35:39 +0100

    Create a marker

--- controllers/marker_ctrl.js
+++ controllers/marker_ctrl.js
@@ -1,7 +1,19 @@
-const { marker_store } = require('../stores');
+const { marker_store } = require('../stores'),
+    { Marker } = require('../models'),
+    team_ctrl = require('./team_ctrl');
+
+let id = 1;
 
 module.exports = {
     getAll: () => {
         return marker_store.getAll();
+    },
+
+    create: (coordinates, isPositive, teamId) => {
+        marker_store.add(
+            new Marker(id, coordinates, isPositive, team_ctrl.getById(teamId))
+        );
+
+        id++;
     }
 };
--- routes/marker_routes.js
+++ routes/marker_routes.js
@@ -4,4 +4,8 @@ module.exports = (io, socket, player) => {
     socket.on('getMarkers', () => {
         socket.emit('getMarkers', marker_ctrl.getAll());
     });
+
+    socket.on('createMarker', ({ coordinates, isPositive }) => {
+        marker_ctrl.create(coordinates, isPositive, player.teamId);
+    });
 };
