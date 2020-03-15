031ca0123fb8cdc70d7bf3eec370e3354221a75a
Clément KOENIG <clement.koenig@unistra.fr>
2020-03-15 13:59:16 +0100

    Get all flags

--- routes/flag_routes.js
+++ routes/flag_routes.js
@@ -1,6 +1,10 @@
 const { flag_ctrl } = require('../controllers');
 
 module.exports = (io, socket, player) => {
+    socket.on('getFlags', () => {
+        socket.emit('getFlags', flag_ctrl.getAll());
+    });
+
     socket.on('captureFlag', ({ flagId, teamId }) => {
         flag_ctrl.captureFlag(flagId, teamId);
     });
