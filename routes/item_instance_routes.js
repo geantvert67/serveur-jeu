a82308105e5784a70ca8eb1b065b67f19ae99fa3
Clément KOENIG <clement.koenig@unistra.fr>
2020-04-25 09:52:43 +0200

    Item => sentinelle V1

--- routes/item_instance_routes.js
+++ routes/item_instance_routes.js
@@ -1,9 +1,11 @@
-const {
-    item_instance_ctrl,
-    item_ctrl,
-    flag_ctrl,
-    player_ctrl
-} = require('../controllers');
+const moment = require('moment'),
+    {
+        item_instance_ctrl,
+        item_ctrl,
+        flag_ctrl,
+        team_ctrl,
+        player_ctrl
+    } = require('../controllers');
 
 module.exports = (io, socket, player) => {
     socket.on('useTempete', id => {
@@ -33,6 +35,27 @@ module.exports = (io, socket, player) => {
                 item_instance_ctrl.removeFromInventory(itemId, player);
                 item_instance_ctrl.delete(id, player);
             }
+        }
+    });
+
+    socket.on('useSentinelle', ({ id, flagId }) => {
+        const flag = flag_ctrl.getById(flagId);
+        const item = item_instance_ctrl.getById(id);
+
+        if (
+            flag &&
+            flag.team &&
+            flag.team.id === team_ctrl.findByPlayer(player.username).id
+        ) {
+            if (flag.capturedUntil) {
+                // TODO : reset timer and create a new one
+            } else {
+                flag.capturedUntil = moment().add(item.effectDuration, 's');
+                setTimeout(() => {
+                    flag.capturedUntil = null;
+                }, item.effectDuration * 1000);
+            }
+            item_instance_ctrl.delete(id, player);
         }
     });
 };
